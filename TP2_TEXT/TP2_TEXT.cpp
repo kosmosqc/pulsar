73a63ee762fd258e84e4314eb1e0d353d58fe6f7
Fairl <Fairl@MAIN>
2021-03-13 17:56:59 -0500

    1

--- TP2_TEXT/TP2_TEXT.cpp
+++ TP2_TEXT/TP2_TEXT.cpp
@@ -9,27 +9,27 @@
 void creeFichier(std::set<Datapoint> dataz);
 int main()
 {
-    std::set<Datapoint> data;
-    std::string id, timestamp, line;
-    double latitude, longitude, temperature;
-    
-    std::ifstream linestream("rawdata.txt");
-    while (!linestream==NULL )
-    {
-      
-            linestream >> latitude >> longitude >> id >> temperature >> timestamp;
-            temperature = (temperature - 32) * (5.0 / 9.0);
-            data.insert(Datapoint(latitude, longitude, temperature, timestamp));
-     
-    }
-    linestream.close();
-    creeFichier(data);
+	std::set<Datapoint> data;
+	std::string id, timestamp, line;
+	double latitude, longitude, temperature;
+
+	std::ifstream linestream("rawdata.txt");
+	while (!linestream == NULL)
+	{
+
+		linestream >> latitude >> longitude >> id >> temperature >> timestamp;
+		temperature = (temperature - 32) * (5.0 / 9.0);
+		data.insert(Datapoint(latitude, longitude, temperature, timestamp));
+
+	}
+	linestream.close();
+	creeFichier(data);
 }
 void creeFichier(std::set<Datapoint> data) {
-    std::ofstream outfile("candata.txt");
-    std::set<Datapoint>::iterator it;
-    for (it = data.begin(); it != data.end(); ++it) {
-        std::cout << it->getTime() << " " << it->getLattitude() << " " << it->getLongitude() << " " << it->getTemp() << std::endl;
-    }
-    outfile.close();
+	std::ofstream outfile("candata.txt");
+	std::set<Datapoint>::iterator it;
+	for (it = data.begin(); it != data.end(); ++it) {
+		std::cout << it->getTime() << " " << it->getLattitude() << " " << it->getLongitude() << " " << it->getTemp() << std::endl;
+	}
+	outfile.close();
 }
