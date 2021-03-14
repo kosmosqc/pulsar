2046e9ad2b4be02b09342e05e7b46ddb4ca07293
Fairl <Fairl@MAIN>
2021-03-13 21:00:05 -0500

    work

--- TP2_TEXT/TP2_TEXT.cpp
+++ TP2_TEXT/TP2_TEXT.cpp
@@ -3,25 +3,41 @@
 #include <fstream>
 #include <set>
 #include <algorithm>
-#include <string>
+
 #include "Datapoint.h"
 
 void creeFichier(std::set<Datapoint> dataz);
 int main()
 {
 	std::set<Datapoint> data;
-	std::string id, timestamp, line;
+	std::string id, timestamp, line, line2;
 	double latitude, longitude, temperature;
-
+	std::ifstream test("rawdata.txt");
 	std::ifstream linestream("rawdata.txt");
 	while (!linestream == NULL)
 	{
-
-		linestream >> latitude >> longitude >> id >> temperature >> timestamp;
-		temperature = (temperature - 32) * (5.0 / 9.0);
-		data.insert(Datapoint(latitude, longitude, temperature, timestamp));
+		
+			getline(test, line);
+			try {
+			//std::cout << line << " **length= "<< line.length() << std::endl;
+			if (line.length() < 25 && line.length() > 0) {
+				linestream >> latitude >> longitude >> id;
+				throw 1;
+				
+			}
+			else {
+				linestream >> latitude >> longitude >> id >> temperature >> timestamp;
+				temperature = (temperature - 32) * (5.0 / 9.0);
+				//	std::cout << latitude << longitude << id << temperature << timestamp <<" &&&&" <<std::endl;
+				data.insert(Datapoint(latitude, longitude, temperature, timestamp));
+			}
+		}
+		catch (const int& code) {
+			std::cout << "Erreur une ligne incomplete a ete ignore " << std::endl;
+		}
 
 	}
+	test.close();
 	linestream.close();
 	creeFichier(data);
 }
@@ -29,7 +45,7 @@ void creeFichier(std::set<Datapoint> data) {
 	std::ofstream outfile("candata.txt");
 	std::set<Datapoint>::iterator it;
 	for (it = data.begin(); it != data.end(); ++it) {
-		std::cout << it->getTime() << " " << it->getLattitude() << " " << it->getLongitude() << " " << it->getTemp() << std::endl;
+		outfile << it->getTime() << " " << it->getLattitude() << " " << it->getLongitude() << " " << it->getTemp() << std::endl;
 	}
 	outfile.close();
 }
